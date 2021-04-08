89afd6d8ed3a04c30dafd769d720c77f3d29ac34
ghitaaitbentaib <>
2021-04-08 15:22:39 +0000

    Exercice Palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -2,6 +2,15 @@ public class Palindrome{
     public static boolean is_palindrome(String s)
     {
         // add your code here
-        return true;
+        int b = s.length();
+        for(int i=0;i<(b-1);i++){
+          if(s.charAt(i)!=s.charAt(b-i-1)){
+            return false;
+          }
+        } 
+        return true;    
+          }
+        
+        
     }
-}
+
